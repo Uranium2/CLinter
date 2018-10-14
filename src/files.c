f54ab597a0e16ed3d211b826ee73390f5acedb7e
Antoine Tavernier <tavernier.antoine@hotmail.fr>
2018-10-15 00:12:35 +0200

    Passing nbLines in getAllLines

--- src/files.c
+++ src/files.c
@@ -12,14 +12,14 @@ int countLinesInFile(FILE* f) {
 }
 
 
-char** getAllLines(char* path) {
+char** getAllLines(char* path, int* nbLines) {
 	FILE* f = fopen(path, "r");
 	if (f == NULL)
 		exit_m("getAllLines: f == NULL");
 	char* line = NULL;
 	size_t len = 0;
-	int nbLines = countLinesInFile(f);
-	char** text = malloc(sizeof(char*) * nbLines);
+	*nbLines = countLinesInFile(f);
+	char** text = malloc(sizeof(char*) * *nbLines);
 	int index = 0;
 	rewind(f); // reset pointer to start of file
 	while ((getline(&line, &len, f)) != -1) {
--- src/main.c
+++ src/main.c
@@ -5,7 +5,8 @@ int main(int argc, char* argv[]) {
 	argv = argv;
 	parse("int a = 10 + 34;");
 	parse("int a=10+34");
-	char** codeText = getAllLines("README.md");
+	int nbLines = 0;
+	char** codeText = getAllLines("README.md", &nbLines);
 	for (int i = 0; i < 2; i++)
 		printf("%s", codeText[i]);
 	return 0;
