42a017c9f41ff1bd01fa7fd6bbf41bd86b10d946
Antoine Tavernier <antoine.tavernier@epita.fr>
2018-10-15 12:29:35 +0200

    Sample file to test Linter

--- src/main.c
+++ src/main.c
@@ -4,10 +4,12 @@ int main(int argc, char* argv[]) {
 	argc = argc;
 	argv = argv;
 	parse("int a = 10 + 34;");
-	parse("int a=10+34");
+	//parse("int a=10+34");
 	int nbLines = 0;
-	char** codeText = getAllLines("README.md", &nbLines);
-	for (int i = 0; i < 2; i++)
+	char** codeText = getAllLines("sample.c", &nbLines);
+	for (int i = 0; i < 2; i++) {
+		parse(codeText[i]);
 		printf("%s", codeText[i]);
+	}
 	return 0;
 }
