7c5b0733fa05ca58941d13f2be4a04e4b3c94346
A4742849 <tavernier.antoine@hotmail.fr>
2018-11-16 09:29:31 +0100

    Files for testing header comments

---
+++ test/bad/comments-header.c
@@ -0,0 +1,3 @@
+int main(int argc, const *char, const *argv) {
+    return 0;
+}
---
+++ test/good/comments-header.c
@@ -0,0 +1,9 @@
+/*****************************************************************
+*
+*                           THIS IS A HEADER COMMENT
+*
+*/
+
+int main(int argc, const *char, const *argv) {
+    return 0;
+}
