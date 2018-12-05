12ce35df879f85efbfc1e49f4d31a04afc6f516b
A4742849 <tavernier.antoine@hotmail.fr>
2018-12-05 12:46:53 +0100

    Fix comment header with new comment gestion '/*' type Nothing

--- src/rules/checkCommentsHeader.c
+++ src/rules/checkCommentsHeader.c
@@ -14,15 +14,18 @@
  * @param lastLine Helps to trigger the warning message at end of file
  * @param fileName File name we are working on
  */
-void checkCommentsHeader(Token **listToken, int nbToken, int line, char *fileName, int *status, int lastLine) {
-    if (line - 1 == lastLine && *status < 2) {
+void checkCommentsHeader(Token **listToken, int nbToken, int line, char *fileName, int *status, int lastLine)
+{
+    if (line - 1 == lastLine && *status < 2)
+    {
         print_warning("Missing header", 1, 0, fileName);
         return;
     }
     if (line > 5 || *status > 1)
         return;
-    for (int i = 0; i < nbToken - 1; i++) {
-        if (*status == 0 && strcmp(listToken[i]->value, "/") == 0 && strcmp(listToken[i + 1]->value, "*") == 0)
+    for (int i = 0; i < nbToken; i++)
+    {
+        if (*status == 0 && listToken[i]->type == Nothing)
             *status = *status + 1;
         else if (*status == 1 && strcmp(listToken[i]->value, "*") == 0 && strcmp(listToken[i + 1]->value, "/") == 0)
             *status = *status + 1;
