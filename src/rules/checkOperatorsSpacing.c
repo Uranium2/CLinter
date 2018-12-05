2b772ab6f4c60c568332289b539ebee50dbd3b12
A4742849 <tavernier.antoine@hotmail.fr>
2018-12-05 12:11:15 +0100

    IDENTIFIER or Numercial in CheckOperators

--- src/rules/checkOperatorsSpacing.c
+++ src/rules/checkOperatorsSpacing.c
@@ -14,18 +14,20 @@
  * @param line Line in the file
  * @param fileName File name we are working on
  */
-void checkOperator(Token **listToken, int nbToken, int line, char *fileName) {
+void checkOperator(Token **listToken, int nbToken, int line, char *fileName)
+{
     if (nbToken == 1)
         return;
-    for (int i = 1; i < nbToken; i++) {
+    for (int i = 1; i < nbToken; i++)
+    {
         if (listToken[i]->type == Operator && strcmp(listToken[i - 1]->value, " ") != 0 &&
             listToken[i - 1]->type != Operator && listToken[i - 1]->type != KeyWord)
             print_warning("Missing space", line, listToken[i]->pos, fileName);
         if ((listToken[i]->type == IDENTIFIER || listToken[i]->type == Numerical) && listToken[i - 1]->type == Operator)
             print_warning("Missing space", line, listToken[i]->pos, fileName);
-        if (listToken[i]->type == EQ_OP && listToken[i - 1]->type == IDENTIFIER)
+        if (listToken[i]->type == EQ_OP && (listToken[i - 1]->type == IDENTIFIER || listToken[i]->type == Numerical))
             print_warning("Missing space", line, listToken[i]->pos, fileName);
-        if (listToken[i - 1]->type == EQ_OP && listToken[i]->type == IDENTIFIER)
+        if (listToken[i - 1]->type == EQ_OP && (listToken[i]->type == IDENTIFIER || listToken[i]->type == Numerical))
             print_warning("Missing space", line, listToken[i]->pos, fileName);
     }
 }
